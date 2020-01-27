70f80a79cf4adf68427beae4267f0c09d591486a
pcornejo2020 <pjaviercornejo@gmail.com>
2020-01-27 12:01:58 -0500

    cambio prueba

--- marylove/src/marylove/Marylove.java
+++ marylove/src/marylove/Marylove.java
@@ -11,7 +11,7 @@ import marylove.vista.FrmLogin;
 import marylove.vista.VistaMenuPrincipal;
 /**
  *
- * @author Patricio, modificación
+ * @author Patricio, modificaciónasdasd
  */
 public class Marylove {
 
