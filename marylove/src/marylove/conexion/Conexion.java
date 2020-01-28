84ac68cee6c032f4fd58030bbdba265ecc89d8e3
Danicho23 <henry.brito4040@gmail.com>
2020-01-28 14:23:40 -0500

    prueba

--- marylove/src/marylove/conexion/Conexion.java
+++ marylove/src/marylove/conexion/Conexion.java
@@ -8,7 +8,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modelo;
+package marylove.conexion;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
