6f2ed029bc84ba64d1055285e572c44e9ae3156d
USUARIO <USUARIO@HP>
2020-01-29 08:22:30 -0500

    modificando

--- marylove/src/marylove/controlador/Controlador_registro_llamadas.java
+++ marylove/src/marylove/controlador/Controlador_registro_llamadas.java
@@ -1,10 +1,13 @@
-
 package marylove.controlador;
+
+import marylove.vista.VistaRegistroLlamadas;
 
 /**
  *
  * @author USUARIO
  */
 public class Controlador_registro_llamadas {
+    VistaRegistroLlamadas vistaRegis_Llamadas;
+    
     
 }
--- marylove/src/marylove/vista/IngresoAvances.java
+++ marylove/src/marylove/vista/IngresoAvances.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package marylove.vista;
 
 import com.toedter.calendar.JDateChooser;
@@ -18,6 +13,7 @@ public class IngresoAvances extends javax.swing.JFrame {
     /**
      * Creates new form IngresoAvances
      */
+    
     public IngresoAvances() {
         initComponents();
     }
