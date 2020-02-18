eef287a487c2694508481382f843191ce6b10d08
Alumno <Alumno@DESKTOP-PQQRBIT>
2020-02-18 15:02:45 -0500

    arreglo de ventana formularioR1 agregado de scrollbar DC

--- marylove/src/marylove/vista/formularioR1.java
+++ marylove/src/marylove/vista/formularioR1.java
@@ -577,7 +577,7 @@ public class formularioR1 extends javax.swing.JFrame {
                     .addGroup(pnlfr1Layout.createSequentialGroup()
                         .addGap(209, 209, 209)
                         .addComponent(jLabel5)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         pnlfr1Layout.setVerticalGroup(
             pnlfr1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -597,8 +597,7 @@ public class formularioR1 extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(pnlfr1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addComponent(pnlfr1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
