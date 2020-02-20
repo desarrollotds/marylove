3b32f7c59b05d7d85fd1bbab8cf8f0620c70b403
Usuario <Usuario@LAPTOP-FEJJVDGB>
2020-02-20 11:37:41 -0500

    Nombre cambiado "marylove.vista.DBmodelo" a "marylove.DBmodelo"

--- marylove/src/marylove/DBmodelo/EgresoDB.java
+++ marylove/src/marylove/DBmodelo/EgresoDB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package marylove.vista.DBmodelo;
+package marylove.DBmodelo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
