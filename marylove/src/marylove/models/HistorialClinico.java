38f71af11e53379561965483b14b82d6349cfa12
Danicho23 <henry.brito4040@gmail.com>
2020-02-28 12:00:56 -0500

    test

---
+++ marylove/src/marylove/models/HistorialClinico.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marylove.models;
+
+/**
+ *
+ * @author LENOVO
+ */
+//nuevo modelo de historial clinico
+public class HistorialClinico {
+    private int hist_id;
+    private int victima_codigo;
+    private String motivo_consulta;
+    private String demanda;
+    private String demanda_implicita;
+    private String historial_violencia;
+    private String biog_psico_perso;
+    private String genograma_famili;
+    private String prub_descripcion;
+    private String apart_gene_conduct;
+    private String conducta;
+    private String func_cogni_sensorio;
+    private String estado_consiencia;
+    private String orientacion;
+    private String memoria;
+    private String atencion_concentracion;
+}
