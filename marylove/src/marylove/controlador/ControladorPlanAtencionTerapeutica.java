1febfd0b80c5e0308d21fc1a043cc9dfe0b6ab43
Danicho23 <henry.brito4040@gmail.com>
2020-02-29 10:19:16 -0500

    completar el controlador (mensaje de prueba)

--- marylove/src/marylove/controlador/ControladorPlanAtencionTerapeutica.java
+++ marylove/src/marylove/controlador/ControladorPlanAtencionTerapeutica.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//falta completar el controlador
 package marylove.controlador;
 
 import static com.sun.tools.javac.util.Constants.format;
