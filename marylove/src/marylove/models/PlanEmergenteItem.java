88bbf043cdcbd2a7bcde211a1d8fcde2b0b7b242
cristopher247 <57427416+cristopher247@users.noreply.github.com>
2020-03-04 15:03:36 -0500

    carapeta iconos

--- marylove/src/marylove/models/PlanEmergenteItem.java
+++ marylove/src/marylove/models/PlanEmergenteItem.java
@@ -13,7 +13,7 @@ import java.util.Date;
  */
 public class PlanEmergenteItem extends PlanEmergente {
     private long item_id;
-    private int emergencia_id;
+    private int emergente_id;
     private String apreciacioninicial;
     private Date item_feha;
     private String modalidad_nombre;
@@ -24,7 +24,7 @@ public class PlanEmergenteItem extends PlanEmergente {
     public PlanEmergenteItem(long item_id, int emergencia_id, String apreciacioninicial, Date item_feha, String modalidad_nombre, long emergente_id, int victima_codigo, Date emergente_fecha, int personal_codigo) {
         super(emergente_id, victima_codigo, emergente_fecha, personal_codigo);
         this.item_id = item_id;
-        this.emergencia_id = emergencia_id;
+        this.emergente_id = emergencia_id;
         this.apreciacioninicial = apreciacioninicial;
         this.item_feha = item_feha;
         this.modalidad_nombre = modalidad_nombre;
@@ -39,11 +39,11 @@ public class PlanEmergenteItem extends PlanEmergente {
     }
 
     public int getEmergencia_id() {
-        return emergencia_id;
+        return emergente_id;
     }
 
     public void setEmergencia_id(int emergencia_id) {
-        this.emergencia_id = emergencia_id;
+        this.emergente_id = emergencia_id;
     }
 
     public String getApreciacioninicial() {
