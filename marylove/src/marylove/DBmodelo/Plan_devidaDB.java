daad7dc52a47c4092addfb6cf21f988101b1834c
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-03-05 21:12:34 -0500

    creando plan de vida y el de recursos

--- marylove/src/marylove/DBmodelo/Plan_devidaDB.java
+++ marylove/src/marylove/DBmodelo/Plan_devidaDB.java
@@ -1,10 +1,45 @@
 package marylove.DBmodelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import marylove.conexion.Conexion;
+import marylove.models.Plan_de_Vida;
+
 /**
  *
  * @author USUARIO
  */
-public class Plan_devidaDB {
-    
-    
+public class Plan_devidaDB extends Plan_de_Vida {
+
+    PreparedStatement ps;
+    ResultSet re = null;
+    Conexion conectar = new Conexion();
+
+    public Plan_devidaDB() {
+    }
+
+    public Plan_devidaDB(int plan_de_vida_codigo, int victima_codigo, String fecha_elaboracion, String fecha_prox_evaluacion, String comosesiente, String comoseve, String comolegustariasuvida) {
+        super(plan_de_vida_codigo, victima_codigo, fecha_elaboracion, fecha_prox_evaluacion, comosesiente, comoseve, comolegustariasuvida);
+    }
+
+    public boolean Ingresar_Plandevida() {
+        boolean ingreso = true;
+        try {
+            String sql = "INSERT INTO public.plan_vida"
+                    + "(planvida_codigo, victima_codigo, fecha_elaboracion, fecha_evaluacion, comosesiente, comoseve, comolegustariasuvida)";
+            sql += "VALUES ";
+            sql += "(" + getPlan_de_vida_codigo() + "," + getVictima_codigo() + ",'" + getFecha_elaboracion()
+                    + "','" + getFecha_prox_evaluacion() + "','" + getComosesiente() + "','" + getComoseve()
+                    + "','" + getComolegustariasuvida() + "')";
+            ps = conectar.conectarBD().prepareStatement(sql);
+            ps.execute();
+            ingreso = true;
+        } catch (SQLException ex) {
+            System.out.println("Error al ingresar Primer Encuentro: " + ex.getMessage());
+            ingreso = false;
+        }
+        conectar.cerrarConexion();
+        return ingreso;
+    }
 }
---
+++ marylove/src/marylove/controlador/ControladorPlandeVida.java
@@ -0,0 +1,36 @@
+package marylove.controlador;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import marylove.DBmodelo.Plan_devidaDB;
+import marylove.conexion.Conexion;
+import marylove.vista.FichaPlandeVida;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ControladorPlandeVida {
+
+    private final FichaPlandeVida vista;
+    private final Plan_devidaDB modelo;
+    private Conexion conex;
+
+    public ControladorPlandeVida(FichaPlandeVida vista, Plan_devidaDB modelo) {
+        this.vista = vista;
+        this.modelo = modelo;
+        this.vista.setVisible(true);
+    }
+
+    public void iniciarControl() {
+
+        Calendar c2 = new GregorianCalendar();
+        vista.getDtcFech_elaboracion().setCalendar(c2);
+    }
+    public void insertaDatos() {
+        
+    }
+    public void borrarDatos() {
+        
+    }
+}
