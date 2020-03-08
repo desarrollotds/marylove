cb666bfc38c97bd65b2a1ecc39a2ba3e6114d230
LENOVO <LENOVO@DESKTOP-FTMO6EO>
2020-03-08 13:21:38 -0500

    2db

---
+++ marylove/src/marylove/DBmodelo/PlanEmergente2DB.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marylove.DBmodelo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import marylove.conexion.Conexion;
+import marylove.models.PlanEmergente;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PlanEmergente2DB extends PlanEmergente{
+    Conexion conex = new Conexion();
+  PreparedStatement ps;
+    ResultSet re = null;
+    
+   public boolean ingresarPlan2() {
+             boolean ingre = true;  
+       try {
+        
+              String sql = "INSERT INTO public.plan_emergente(emergente_fecha)";
+              sql += "VALUES";
+              sql += " ('"+getEmergente_fecha()+')";
+              ps = conex.conectarBD().prepareStatement(sql);
+              ps.execute();
+              ingre = true;
+              if (conex.noQuery(sql) == null) {
+                  return true;
+              } else {
+                  return false;
+              } } catch (SQLException ex) {
+              System.out.println("error: " +ex);
+          }
+       conex.cerrarConexion();
+       return ingre;
+    }
+    
+}
