e2aa0ad883ef48349169e1be1185bee4a2bc34c6
Asus <Asus@192.168.1.18>
2020-03-09 01:58:26 +0100

    mura

---
+++ marylove/src/marylove/controlador/ControladorAgregarHijos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marylove.controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import marylove.vista.FormaAgregarHijos;
+
+/**
+ *
+ * @author Asus
+ */
+public class ControladorAgregarHijos implements ActionListener {
+    FormaAgregarHijos v;
+    public ControladorAgregarHijos(FormaAgregarHijos v) {
+        this.v=v;
+        this.v.getBtnGuardar().addActionListener(l);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    
+    
+}
