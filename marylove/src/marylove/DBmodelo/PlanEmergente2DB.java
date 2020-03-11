2bb18f3ee393d598f40b5cc79308375812e8a626
cristopher247 <57427416+cristopher247@users.noreply.github.com>
2020-03-11 17:23:01 -0500

    controlador

--- marylove/src/marylove/DBmodelo/PlanEmergente2DB.java
+++ marylove/src/marylove/DBmodelo/PlanEmergente2DB.java
@@ -15,11 +15,12 @@ import marylove.models.PlanEmergenteItem;
 
 /**
  *
- * @author LENOVO
+ * @author LENqweqweqOVO
  */
-public class PlanEmergente2DB extends PlanEmergente{
+public class PlanEmergente2DB extends PlanEmergente {
+
     Conexion conex = new Conexion();
-  PreparedStatement ps;
+    PreparedStatement ps;
     ResultSet re = null;
 
     public PlanEmergente2DB() {
@@ -28,60 +29,55 @@ public class PlanEmergente2DB extends PlanEmergente{
     public PlanEmergente2DB(int victima_codigo, String emergente_fecha, int personal_codigo) {
         super(victima_codigo, emergente_fecha, personal_codigo);
     }
-    
-    
-   public boolean ingresarPlan2() {
-             boolean ingre = true;  
-       try {
-        
-              String sql = "INSERT INTO public.plan_emergente(victima_codigo, emergente_fecha, personal_codigo)";
-              sql += "VALUES";
-              sql += " ('"+getVictima_codigo()+"','"+getEmergente_fecha()+"','"+getPersonal_codigo()+"')";
-              ps = conex.conectarBD().prepareStatement(sql);
-              ps.execute();
-             
-              if (conex.noQuery(sql) == null) {
-                  return true;
-              } else {
-                  return false;
-              } } catch (SQLException ex) {
-              System.out.println("error: " +ex);
-          }
-       conex.cerrarConexion();
-       return ingre;
-    }
-    public PlanEmergente obtenetCV(int ced){
-        PlanEmergente p = new PlanEmergente();
-        PlanEmergenteItem pi = new PlanEmergenteItem();
-//        "SELECT a.item_id, a.emergente_id, a.apreciacioninicial, a.accionesinmediatas, a.item_fecha, a.modalidad_nombre,p.emergente_fecha
-//	FROM plan_emerg_item as a
-//	JOIN plan_emergente as p
-//	on p.emergente_id =a.emergente_id
-//	where p.victima_codigo='"+ced+"';";
+
+    public boolean ingresarPlan2() {
+        boolean ingre = true;
         try {
-           // String sql = "select * from plan_emergente where victima_codigo='"+ced+"';";
-            String sql = "SELECT ,p.emergente_fecha"
-            +"FROM plan_emerg_item as a"
-            +"JOIN plan_emergente as p"
-            +"on p.emergente_id =a.emergente_id"
-            +"where p.victima_codigo='"+ced+"';";
+
+            String sql = "INSERT INTO public.plan_emergente(victima_codigo, emergente_fecha, personal_codigo)";
+            sql += "VALUES";
+            sql += " ('" + getVictima_codigo() + "','" + getEmergente_fecha() + "','" + getPersonal_codigo() + "')";
             ps = conex.conectarBD().prepareStatement(sql);
-            re = ps.executeQuery();
-            while (re.next()) {
-                p.setEmergente_fecha(re.getString(1));
-                p.setVictima_codigo(re.getInt(2));
-               // p.setPersonal_codigo(re.getInt(3));
-//                pi.setAccionesinmediatas(re.getString(4));
-//                pi.setApreciacioninicial(re.getString(5));
-//                pi.setItem_fecha(re.getString(6));
-                
+            ps.execute();
+
+            if (conex.noQuery(sql) == null) {
+                return true;
+            } else {
+                return false;
             }
         } catch (SQLException ex) {
-            System.out.println("error al obtener datos de victima "+ex.getMessage());
+            System.out.println("error: " + ex);
         }
         conex.cerrarConexion();
-        return p;
+        return ingre;
     }
-    
-    
+
+    public void obtenetSelect(int ced) {
+     
+        try {
+            String sql = "SELECT "
+                    + "a.item_id,"
+                    + "a.emergente_id,"
+                    + "p.emergente_id, "
+                    + "a.apreciacioninicial, "
+                    + "a.accionesinmediatas, "
+                    + "a.item_fecha, "
+                    + "a.modalidad_nombre,"
+                    + "p.emergente_fecha,"
+                    + "p.victima_codigo"
+                    + " FROM plan_emerg_item a"
+                    + " JOIN plan_emergente p on p.emergente_id =a.emergente_id"
+                    + " JOIN victima v ON v.victima_codigo = p.victima_codigo"
+                    + " JOIN persona per ON per.persona_codigo = v.persona_codigo"             
+                    + " WHERE per.persona_cedula = '"+ced+"';";
+            ps = conex.conectarBD().prepareStatement(sql);
+            re = ps.executeQuery();
+
+        } catch (SQLException ex) {
+            System.out.println("error al obtener datos de victima mijin  " + ex.getMessage());
+        }
+        conex.cerrarConexion();
+       
+    }
+
 }//sdfsdfsdf
