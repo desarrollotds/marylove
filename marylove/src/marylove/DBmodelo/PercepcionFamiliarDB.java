737bbacedeb74aba73dd51bcbfb6a3fb01d1a37c
WillianPanza11 <57450978+WillianPanza11@users.noreply.github.com>
2020-05-18 16:35:41 -0500

    modificaciones

--- marylove/src/marylove/DBmodelo/PercepcionFamiliarDB.java
+++ marylove/src/marylove/DBmodelo/PercepcionFamiliarDB.java
@@ -9,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import marylove.conexion.Conexion;
 import marylove.conexion.ConexionHi;
+import marylove.models.EvaluacionPlanVida;
 import marylove.models.PercepcionFamiliar;
 
 public class PercepcionFamiliarDB extends PercepcionFamiliar {
@@ -37,13 +38,13 @@ public class PercepcionFamiliarDB extends PercepcionFamiliar {
             return false;
         }
     }
-    
+
     public List<PercepcionFamiliar> listarPerFam() throws SQLException {
         List<PercepcionFamiliar> listarPerFam = new ArrayList<PercepcionFamiliar>();
-        String sql = "select percepcion_id,comoseseinte,alcanzoonjetivos_como,dificultadesencontradas from percepcion_familiar pf\n" +
-                      "join evaluacion_plan_vida epv\n" +
-                      "on epv.evaluacion_id = pf.evaluacion_id\n" +
-                      "where epv.victima_codigo = '1';";
+        String sql = "select percepcion_id,comoseseinte,alcanzoonjetivos_como,dificultadesencontradas from percepcion_familiar pf\n"
+                + "join evaluacion_plan_vida epv\n"
+                + "on epv.evaluacion_id = pf.evaluacion_id\n"
+                + "where epv.victima_codigo = '1';";
 //        sql += "order by 1";
         ResultSet rs = con.query(sql);
         try {
@@ -63,18 +64,48 @@ public class PercepcionFamiliarDB extends PercepcionFamiliar {
         }
 
     }
-    
+
     public boolean actualizarPerFam() {
         String sql = "UPDATE percepcion_familiar SET ";
-        sql += "comoseseinte='" + getComoSeSiente()+ "', ";
-        sql += "alcanzoonjetivos_como='" + getAlcanzoObjetivosComo()+ "', ";
-        sql += "dificultadesencontradas='" + getDificultadesEncontradas()+ "'";
-        sql += " WHERE percepcion_id='" + getPercepcion_id()+ "'";
+        sql += "comoseseinte='" + getComoSeSiente() + "', ";
+        sql += "alcanzoonjetivos_como='" + getAlcanzoObjetivosComo() + "', ";
+        sql += "dificultadesencontradas='" + getDificultadesEncontradas() + "'";
+        sql += " WHERE percepcion_id='" + getPercepcion_id() + "'";
 
         if (con.noQuery(sql) == null) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public List<PercepcionFamiliar> buscarTextoPercepcionFam(String texto) throws SQLException {
+        List<PercepcionFamiliar> buscarTexto = new ArrayList();
+        System.out.println("testoDB: "+texto);
+        String sql = "select* from percepcion_familiar pf\n"
+                + "join evaluacion_plan_vida epv\n"
+                + "on pf.evaluacion_id = epv.evaluacion_id join victima vic\n"
+                + "on vic.victima_codigo= epv.victima_codigo join persona as pe\n"
+                + "on vic.persona_codigo =  pe.persona_codigo\n"
+                + "where persona_cedula like '"+texto+"%'\n"
+                + "OR persona_nombre LIKE '"+texto+"%'\n"
+                + "OR persona_apellido like '"+texto+"%';";
+        ResultSet rs = con.query(sql);
+        try {
+            while (rs.next()) {
+                PercepcionFamiliar p = new PercepcionFamiliar();
+                p.setPercepcion_id(rs.getInt("percepcion_id"));
+                p.setComoSeSiente(rs.getString("comoseseinte"));
+                p.setAlcanzoObjetivosComo(rs.getString("alcanzoonjetivos_como"));
+                p.setDificultadesEncontradas(rs.getString("dificultadesencontradas"));
+                buscarTexto.add(p);
+
+            }
+            rs.close();
+            return buscarTexto;
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
     }
 }
