438dceab02db8437645e584e9ce28246cd354ac2
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-05-19 08:36:37 -0500

    plan de recursos listas

--- marylove/src/marylove/DBmodelo/Cuentas_DiariasDB.java
+++ marylove/src/marylove/DBmodelo/Cuentas_DiariasDB.java
@@ -3,6 +3,10 @@ package marylove.DBmodelo;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import marylove.conexion.ConexionHi;
 import marylove.models.Cuentas_Diarias;
 
@@ -26,9 +30,9 @@ public class Cuentas_DiariasDB extends Cuentas_Diarias{
         boolean ingreso = true;
         try {
             String sql = "INSERT INTO public.cuentas_diarias"
-                    + "(cuentas_codigo, planrecursos_codigo, fecha_cuenta, gasto, descripcion_gasto, saldo)";
+                    + "( planrecursos_codigo, fecha_cuenta, gasto, descripcion_gasto, saldo)";
             sql += "VALUES ";
-            sql += "(" + getCuentas_diarias_codigo()+ "," + getPlan_recusos_codigo()+ ",'" + getFecha_cuenta()
+            sql += "(" + getPlan_recusos_codigo()+ ",'" + getFecha_cuenta()
                     + "','" + getGasto()+ "','" + getDescripcion()+ "','" + getSaldo()+ "')";
             ps = conectar.getConnection().prepareStatement(sql);
             ps.execute();
@@ -39,6 +43,47 @@ public class Cuentas_DiariasDB extends Cuentas_Diarias{
         }
         conectar.cerrarConexion();
         return ingreso;
+    }
+    public List<Cuentas_Diarias> listacuentasDiarias(int cod) throws SQLException {
+        List<Cuentas_Diarias> listacuentasDiarias = new ArrayList<Cuentas_Diarias>();
+        String sql = "select * from cuentas_diarias cutd\n"
+                + "join plan_recursos plr\n"
+                + "on cutd.planrecursos_codigo = plr.planrecursos_codigo\n"
+                + "where pv.victima_codigo = '" + cod + "';";
+//        sql += "order by 1";
+        ResultSet rs = conectar.query(sql);
+        try {
+            while (rs.next()) {
+                Cuentas_Diarias cutd = new Cuentas_Diarias();
+                cutd.setCuentas_diarias_codigo(rs.getInt("cuentas_codigo"));
+                //planrecursos_codigo
+                cutd.setFecha_cuenta(rs.getString("fecha_cuenta"));
+                cutd.setGasto(rs.getString("gasto"));
+                cutd.setDescripcion(rs.getString("descripcion_gasto"));
+                cutd.setSaldo(rs.getString("saldo"));
+                listacuentasDiarias.add(cutd);
+            }
+            rs.close();
+            return listacuentasDiarias;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionHi.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public boolean actualizarCuentasDiarias() {
+        String sql = "UPDATE cuentas_diarias SET ";
+        sql += "fecha_cuenta='" + getFecha_cuenta()+ "', ";
+        System.out.println("objet: " + getFecha_cuenta());
+        sql += "gasto='" + getGasto()+ "', ";
+        sql += "descripcion_gasto='" + getDescripcion()+ "',";
+        sql += "saldo='" + getSaldo()+ "',";
+        sql += " WHERE cuentas_codigo='" + getCuentas_diarias_codigo()+ "';";
+
+        if (conectar.noQuery(sql) == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
 }
--- marylove/src/marylove/DBmodelo/Monto_NecesitaDB.java
+++ marylove/src/marylove/DBmodelo/Monto_NecesitaDB.java
@@ -3,6 +3,10 @@ package marylove.DBmodelo;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import marylove.conexion.ConexionHi;
 import marylove.models.Monto_Necesita;
 
@@ -39,6 +43,47 @@ public class Monto_NecesitaDB extends Monto_Necesita{
         }
         conectar.cerrarConexion();
         return ingreso;
+    }
+    public List<Monto_Necesita> listaMontoNecesita(int cod) throws SQLException {
+        List<Monto_Necesita> listaMontoNecesita = new ArrayList<Monto_Necesita>();
+        String sql = "select * from monto_necesita mn\n"
+                + "join plan_recursos plr\n"
+                + "on mn.planrecursos_codigo = plr.planrecursos_codigo\n"
+                + "where pv.victima_codigo = '" + cod + "';";
+//        sql += "order by 1";
+        ResultSet rs = conectar.query(sql);
+        try {
+            while (rs.next()) {
+                Monto_Necesita mtn = new Monto_Necesita();
+                mtn.setMonto_nesecita_codigo(rs.getInt("monto_codigo"));
+                //planrecursos_codigo
+                mtn.setVivienda_monto(rs.getString("vivienda"));
+                mtn.setAlimentacion_monto(rs.getString("alimentcion"));
+                mtn.setEducacion_monto(rs.getString("educacion"));
+                mtn.setTransporte_monto(rs.getString("transporte"));
+                listaMontoNecesita.add(mtn);
+            }
+            rs.close();
+            return listaMontoNecesita;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionHi.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public boolean actualizarMontoDisponible() {
+        String sql = "UPDATE monto_necesita SET ";
+        sql += "vivienda='" + getVivienda_monto()+ "', ";
+        System.out.println("objet: " + getVivienda_monto());
+        sql += "alimentcion='" + getAlimentacion_monto()+ "', ";
+        sql += "educacion='" + getEducacion_monto()+ "',";
+        sql += "transporte='" + getTransporte_monto()+ "',";
+        sql += " WHERE monto_codigo='" + getMonto_nesecita_codigo()+ "';";
+
+        if (conectar.noQuery(sql) == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
 }
