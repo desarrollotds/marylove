7e62b4ef6892ee14ff40e70f576e8354317178f7
Acer <Acer@DESKTOP-7FFGIGD>
2020-05-20 12:53:54 -0500

    cambio

--- marylove/src/marylove/models/PlanEmergenteItem.java
+++ marylove/src/marylove/models/PlanEmergenteItem.java
@@ -12,23 +12,126 @@ import java.util.Date;
  * @autsdefssddshor Alumno
  */
 public class PlanEmergenteItem  {
-    private long item_id;
+   private long item_id;
 
     
     private String apreciacioninicial;
+    private String apreciacioninicial1;
     private String accionesinmediatas;
+    private String accionesinmediatas1;
     private String item_fecha;
+    private String item_fecha1;
+     private String apreciacioninicial2;
+    private String apreciacioninicial3;
+    private String accionesinmediatas2;
+    private String accionesinmediatas3;
+    private String item_fecha2;
+    private String item_fecha3;
     private String modalidad_nombre;
 
     public PlanEmergenteItem() {
     }
 
-    public PlanEmergenteItem(long item_id, String apreciacioninicial, String accionesinmediatas, String item_fecha, String modalidad_nombre, int victima_codigo, String emergente_fecha, int personal_codigo) {
+  
+
+    public PlanEmergenteItem(long item_id, String apreciacioninicial, String apreciacioninicial1, String accionesinmediatas, String accionesinmediatas1, String item_fecha, String item_fecha1, String modalidad_nombre) {
         this.item_id = item_id;
         this.apreciacioninicial = apreciacioninicial;
+        this.apreciacioninicial1 = apreciacioninicial1;
         this.accionesinmediatas = accionesinmediatas;
+        this.accionesinmediatas1 = accionesinmediatas1;
         this.item_fecha = item_fecha;
+        this.item_fecha1 = item_fecha1;
         this.modalidad_nombre = modalidad_nombre;
+    }
+
+    public PlanEmergenteItem(long item_id, String apreciacioninicial, String apreciacioninicial1, String accionesinmediatas, String accionesinmediatas1, String item_fecha, String item_fecha1, String apreciacioninicial2, String apreciacioninicial3, String accionesinmediatas2, String accionesinmediatas3, String item_fecha2, String item_fecha3, String modalidad_nombre) {
+        this.item_id = item_id;
+        this.apreciacioninicial = apreciacioninicial;
+        this.apreciacioninicial1 = apreciacioninicial1;
+        this.accionesinmediatas = accionesinmediatas;
+        this.accionesinmediatas1 = accionesinmediatas1;
+        this.item_fecha = item_fecha;
+        this.item_fecha1 = item_fecha1;
+        this.apreciacioninicial2 = apreciacioninicial2;
+        this.apreciacioninicial3 = apreciacioninicial3;
+        this.accionesinmediatas2 = accionesinmediatas2;
+        this.accionesinmediatas3 = accionesinmediatas3;
+        this.item_fecha2 = item_fecha2;
+        this.item_fecha3 = item_fecha3;
+        this.modalidad_nombre = modalidad_nombre;
+    }
+
+    public String getApreciacioninicial2() {
+        return apreciacioninicial2;
+    }
+
+    public void setApreciacioninicial2(String apreciacioninicial2) {
+        this.apreciacioninicial2 = apreciacioninicial2;
+    }
+
+    public String getApreciacioninicial3() {
+        return apreciacioninicial3;
+    }
+
+    public void setApreciacioninicial3(String apreciacioninicial3) {
+        this.apreciacioninicial3 = apreciacioninicial3;
+    }
+
+    public String getAccionesinmediatas2() {
+        return accionesinmediatas2;
+    }
+
+    public void setAccionesinmediatas2(String accionesinmediatas2) {
+        this.accionesinmediatas2 = accionesinmediatas2;
+    }
+
+    public String getAccionesinmediatas3() {
+        return accionesinmediatas3;
+    }
+
+    public void setAccionesinmediatas3(String accionesinmediatas3) {
+        this.accionesinmediatas3 = accionesinmediatas3;
+    }
+
+    public String getItem_fecha2() {
+        return item_fecha2;
+    }
+
+    public void setItem_fecha2(String item_fecha2) {
+        this.item_fecha2 = item_fecha2;
+    }
+
+    public String getItem_fecha3() {
+        return item_fecha3;
+    }
+
+    public void setItem_fecha3(String item_fecha3) {
+        this.item_fecha3 = item_fecha3;
+    }
+
+    public String getApreciacioninicial1() {
+        return apreciacioninicial1;
+    }
+
+    public void setApreciacioninicial1(String apreciacioninicial1) {
+        this.apreciacioninicial1 = apreciacioninicial1;
+    }
+
+    public String getAccionesinmediatas1() {
+        return accionesinmediatas1;
+    }
+
+    public void setAccionesinmediatas1(String accionesinmediatas1) {
+        this.accionesinmediatas1 = accionesinmediatas1;
+    }
+
+    public String getItem_fecha1() {
+        return item_fecha1;
+    }
+
+    public void setItem_fecha1(String item_fecha1) {
+        this.item_fecha1 = item_fecha1;
     }
 
     
