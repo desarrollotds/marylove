6826047c10e91135737f1c8375308706b376ed8b
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-05-22 17:00:03 -0500

    Update testManuel.java

--- marylove/src/marylove/testManuel.java
+++ marylove/src/marylove/testManuel.java
@@ -6,17 +6,25 @@ import marylove.DBmodelo.Monto_DisponeDB;
 import marylove.DBmodelo.Monto_NecesitaDB;
 import marylove.DBmodelo.PlanAutonomiaDB;
 import marylove.DBmodelo.Plan_deRecursosDB;
+import marylove.DBmodelo.PvObjetivosEspecDB;
+import marylove.DBmodelo.PvObjetivosGeneDB;
 import marylove.DBmodelo.primer_EncuentroDB;
 import marylove.controlador.ControladorPlandeRecursos;
+import marylove.controlador.ControladorPlandeVida;
 import marylove.controlador.ControladorPrimerEncuentro;
 import marylove.controlador.controlPlanAutonomia;
 import marylove.models.Cuentas_Diarias;
 import marylove.models.Monto_Dispone;
 import marylove.models.Monto_Necesita;
 import marylove.models.Plan_Autonomia;
+import marylove.models.Pv_objeticos_especificos;
+import marylove.models.Pv_objetivos_gene;
+import marylove.vista.FichaPlandeVida;
 import marylove.vista.FichaPrimerEncuentro;
+import marylove.vista.VistaDefinicionObjetivosEspecifico;
 import marylove.vista.VistaPlanAutonomía;
 import marylove.vista.VistaPlanRecursos;
+import marylove.vista.vistaAgregarObjetivoGenera;
 import marylove.vista.vistaCuentasDiarias;
 import marylove.vista.vistaGastosPrioritarios;
 
@@ -27,18 +35,29 @@ import marylove.vista.vistaGastosPrioritarios;
 public class testManuel {
     public static void main(String[] args) throws SQLException, Exception {
         
-     VistaPlanRecursos vpr = new VistaPlanRecursos();
-     Monto_DisponeDB montDispModlDB = new Monto_DisponeDB();
-     Monto_NecesitaDB montNecesModelDB = new Monto_NecesitaDB();
-     Cuentas_DiariasDB cuentDiariasModelDB = new Cuentas_DiariasDB();
-     Monto_Dispone montDMdel = new Monto_Dispone();
-     Monto_Necesita montNMdel = new Monto_Necesita();
-     Cuentas_Diarias cuentDMdel = new Cuentas_Diarias();
-     vistaCuentasDiarias vistCuentD = new vistaCuentasDiarias();
-     vistaGastosPrioritarios vistGastPrio = new vistaGastosPrioritarios();          
-    ControladorPlandeRecursos contR = new ControladorPlandeRecursos(vpr, montDispModlDB, montNecesModelDB, cuentDiariasModelDB, montDMdel, montNMdel, cuentDMdel, vistCuentD, vistGastPrio);
-    contR.iniciarControlRecursos();
-    vpr.setVisible(true);
+//     VistaPlanRecursos vpr = new VistaPlanRecursos();
+//     Monto_DisponeDB montDispModlDB = new Monto_DisponeDB();
+//     Monto_NecesitaDB montNecesModelDB = new Monto_NecesitaDB();
+//     Cuentas_DiariasDB cuentDiariasModelDB = new Cuentas_DiariasDB();
+//     Monto_Dispone montDMdel = new Monto_Dispone();
+//     Monto_Necesita montNMdel = new Monto_Necesita();
+//     Cuentas_Diarias cuentDMdel = new Cuentas_Diarias();
+//     vistaCuentasDiarias vistCuentD = new vistaCuentasDiarias();
+//     vistaGastosPrioritarios vistGastPrio = new vistaGastosPrioritarios();          
+//    ControladorPlandeRecursos contR = new ControladorPlandeRecursos(vpr, montDispModlDB, montNecesModelDB, cuentDiariasModelDB, montDMdel, montNMdel, cuentDMdel, vistCuentD, vistGastPrio);
+//    contR.iniciarControlRecursos();
+//    vpr.setVisible(true);
+
+    FichaPlandeVida vPVida = new FichaPlandeVida();
+    PvObjetivosGeneDB objGenModlDB = new PvObjetivosGeneDB();
+    PvObjetivosEspecDB objEspeModelDB = new PvObjetivosEspecDB();
+    Pv_objeticos_especificos objEspeMdel = new Pv_objeticos_especificos();
+    Pv_objetivos_gene objGeMOdel = new Pv_objetivos_gene();
+    VistaDefinicionObjetivosEspecifico vistObjEsp = new VistaDefinicionObjetivosEspecifico();
+    vistaAgregarObjetivoGenera vistObjGene = new vistaAgregarObjetivoGenera();
+    ControladorPlandeVida contPVida = new ControladorPlandeVida(vPVida, objGenModlDB, objEspeModelDB, objGeMOdel, objEspeMdel, vistObjEsp, vistObjGene);
+     contPVida.iniciarControl();
+     vPVida.setVisible(true);
     
     
     
