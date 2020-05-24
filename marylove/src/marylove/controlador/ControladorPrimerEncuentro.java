8930c485e6abdbc8ae190f7f2f564bf055a8ff62
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-05-23 23:40:15 -0500

    arreglsndo un error en plan de vida

--- marylove/src/marylove/controlador/ControladorPrimerEncuentro.java
+++ marylove/src/marylove/controlador/ControladorPrimerEncuentro.java
@@ -38,33 +38,35 @@ public class ControladorPrimerEncuentro extends Validaciones {
     public void insertaDatos() {
         if (vista_1encuentro.getTxaEstadoEmocional().getText().equals("")
                 || vista_1encuentro.getTxaNivelRiesgo().getText().equals("")
-                || vista_1encuentro.getTxaValoracionDaño().getText().equals("")) {
+                || vista_1encuentro.getTxaNivelRiesgo().getText().equals("")
+                || vista_1encuentro.getTxaInquietudes().getText().equals("")) {
             JOptionPane.showMessageDialog(null, "Llene todos los campos");
         } else {
-            //modelo_1encuentro.setPrimer_codigo(2);
             modelo_1encuentro.setVictima_codigo(Integer.parseInt(vista_1encuentro.getTxtCodigo().getText()));
             modelo_1encuentro.setPstIntCrisis_fecha(obtenerFecha(vista_1encuentro.getDatFechaPrimerEncuentro()));
             modelo_1encuentro.setPstIntCrisis_estado_emocional(vista_1encuentro.getTxaEstadoEmocional().getText());
             modelo_1encuentro.setPstIntCrisis_nivel_riesgo(vista_1encuentro.getTxaNivelRiesgo().getText());
             modelo_1encuentro.setPstIntCrisis_valoracionpreliminar(vista_1encuentro.getTxaValoracionDaño().getText());
-            if (vista_1encuentro.getCmbRiesgo().equals("Si")) {
+            String opc = (String)vista_1encuentro.getCmbRiesgo().getSelectedItem();
+            if (opc.equals("Si")) {
                 modelo_1encuentro.setPstIntCrisis_riesgo_suicida(true);
-            } else if (vista_1encuentro.getCmbRiesgo().equals("No")) {
+            }
+            if (opc.equals("No")) {
                 modelo_1encuentro.setPstIntCrisis_riesgo_suicida(false);
             }
             modelo_1encuentro.setPstIntCrisis_puntosReelevantes(vista_1encuentro.getTxaInquietudes().getText());
             if (vista_1encuentro.getJrbSi().isSelected()) {
                 modelo_1encuentro.setPstIntCrisis_proceso_psicoterapeutico(true);
-
             } else if (vista_1encuentro.getJrbNo().isSelected()) {
                 modelo_1encuentro.setPstIntCrisis_proceso_psicoterapeutico(false);
             }
+            
             if (vista_1encuentro.getJrbProceso().isSelected()) {
                 modelo_1encuentro.setPstIntCrisis_asesoria(true);
-
             } else if (vista_1encuentro.getJrbAsesoria().isSelected()) {
                 modelo_1encuentro.setPstIntCrisis_asesoria(false);
             }
+            
             modelo_1encuentro.setPsicologo_codigo(pDB.verifiUserP(personal_cod));
             
                 if (modelo_1encuentro.Ingresar_PrimerEncuentro() ) {
