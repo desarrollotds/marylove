b5bec03e5737a1f8f1eaba9d0cbd0c80a873052e
Xavier Yanza <xavier.y.2013@gmail.com>
2020-05-24 02:05:35 -0500

    Reportes

--- marylove/src/marylove/controlador/ControlReporte.java
+++ marylove/src/marylove/controlador/ControlReporte.java
@@ -2,6 +2,7 @@ package marylove.controlador;
 
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.FontFactory;
 import com.lowagie.text.PageSize;
@@ -52,7 +53,7 @@ public class ControlReporte implements ActionListener {
     private DefaultComboBoxModel modelo;
     private IngresoDB i;
     private int bandera;
-
+    private Validaciones validaciones;
     private ConexionHi conn;
 
     public ControlReporte(VistaReportes vreportes) {
@@ -137,6 +138,7 @@ public class ControlReporte implements ActionListener {
             }
             if (bandera == 2) {
                 JOptionPane.showMessageDialog(vreportes, "Se llama al método del reporte General");
+                ReporteGeneralVictima();
             }
             if (bandera == 3) {
                 socialReport();
@@ -224,20 +226,293 @@ public class ControlReporte implements ActionListener {
     }
 
     public void Listar() {
-
         try {
             ConexionHi conexion = new ConexionHi();
-            Statement comando = conexion.getConnection().createStatement();
-            ResultSet res = comando.executeQuery("select persona_nombre from persona");
-
+            String sql = "select persona_nombre from persona";
+            ResultSet res = conexion.query(sql);
             while (res.next()) {
                 JOptionPane.showMessageDialog(null, res.getString("persona_nombre"));
             }
-
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
+    }
 
+    public void createGeneralReport() {
+        String titulo = "Reporte General de las compañeras acogidas";
+        String[] cabecera = {"Nombre", "F.Ingreso", "F.Egreso", "Agresor", "Nacionalidad", "Provincia", "Ciudad", "Parroquia", "Años", "Instruccion",
+            "Ocupación", "Estado Civil ", "#NNA", "NNA", "Sexo", "F.Nacimiento", "Años", "Año Escolar", "Institución Educativa"};
+        PdfPTable tabla = createTable(cabecera, 19);
+        float[] medidaCeldas = {2f, 2f, 2f, 2f, 2f, 2f, 2f, 2f, 1f, 2f, 2f, 2f, 1f, 2f, 1f, 2f, 1f, 2f, 2f};
+        try {
+            tabla.setWidths(medidaCeldas);
+        } catch (Exception e) {
+        }
+        try {
+            Document doc = createDocument();
+            doc.add(Ptittle(titulo));
+            doc.add(new Phrase(validaciones.Fecha()));
+            String SQL_SELECT = "SELECT\n"
+                    + " p.persona_nombre||' '|| p.persona_apellido AS \"Compañera\",\n"
+                    + " (CASE\n"
+                    + " WHEN  i.ingreso_fecha  IS NULL THEN ''\n"
+                    + " ELSE i.ingreso_fecha||''\n"
+                    + " END) AS \"Fecha Ingreso\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  e.egreso_fecha  IS NULL THEN ''\n"
+                    + " ELSE e.egreso_fecha||''\n"
+                    + " END) AS \"Fecha Egreso\" ,\n"
+                    + " (select par_valores->xra.parentesco->>'valor' from parametros where par_id = 9) AS \"Agresor\",\n"
+                    + " (select par_valores->p.persona_nacionalidad->>'valor' from parametros where par_id = 3) AS \"Nacionalidad\",\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_provincia  IS NULL THEN ''\n"
+                    + " ELSE d.dir_provincia||''\n"
+                    + " END) AS \"Provincia\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_ciudad  IS NULL THEN ''\n"
+                    + " ELSE d.dir_ciudad||''\n"
+                    + " END) AS \"Ciudad\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_parroquia  IS NULL THEN ''\n"
+                    + " ELSE d.dir_parroquia||''\n"
+                    + " END) AS \"Parroquia\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  ((current_date-p.persona_fecha_nac)/365)  IS NULL THEN ''\n"
+                    + " ELSE ((current_date-p.persona_fecha_nac)/365)||''\n"
+                    + " END) AS \"Años\" ,\n"
+                    + " (select par_valores->p.persona_nivel_acad->>'valor' from parametros where par_id = 5) AS \"Instruccion\",\n"
+                    + " (select par_valores->p.persona_ocupacion->>'valor' from parametros where par_id = 7) AS \"Ocupacion\",\n"
+                    + " (select par_valores->p.persona_estadocivil->>'valor' from parametros where par_id = 4) AS \"Estado Civil\",\n"
+                    + " (SELECT\n"
+                    + " COUNT(*)\n"
+                    + " FROM\n"
+                    + " hijos h \n"
+                    + " WHERE\n"
+                    + " h.victima_codigo =v.victima_codigo) AS \"NNA Responsables\",\n"
+                    + " (CASE\n"
+                    + " WHEN  p1.persona_nombre || ' '||p1.persona_apellido  IS NULL THEN ''\n"
+                    + " ELSE p1.persona_nombre || ' '||p1.persona_apellido\n"
+                    + " END)\n"
+                    + " AS \" NNA\",\n"
+                    + " (CASE\n"
+                    + " WHEN p1.persona_sexo IS NULL THEN ''\n"
+                    + " ELSE p1.persona_sexo||''\n"
+                    + " END) AS \"Sexo\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN p1.persona_fecha_nac  IS NULL THEN ''\n"
+                    + " ELSE p1.persona_fecha_nac||''\n"
+                    + " END) AS \"Fecha de Nacimiento\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  ((current_date-p1.persona_fecha_nac)/365)  IS NULL THEN ''\n"
+                    + " ELSE ((current_date-p1.persona_fecha_nac)/365)||''\n"
+                    + " END) AS \"Años NNA \" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  h.hijo_anioescolar IS NULL THEN ''\n"
+                    + " ELSE  h.hijo_anioescolar||''\n"
+                    + " END) AS \"Año Escolar\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN ie.inst_nombre IS NULL THEN ''\n"
+                    + " ELSE ie.inst_nombre||''\n"
+                    + " END) AS \"Institucion Educativa\" \n"
+                    + "\n"
+                    + " FROM\n"
+                    + " persona p\n"
+                    + " JOIN \n"
+                    + " victima v\n"
+                    + " ON \n"
+                    + " v.persona_codigo = p.persona_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " ingreso i\n"
+                    + " ON \n"
+                    + " i.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " egreso e\n"
+                    + " ON\n"
+                    + " e.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " direccion_persona dp\n"
+                    + " ON\n"
+                    + " p.persona_codigo = dp.persona_codigo\n"
+                    + " LEFT JOIN \n"
+                    + " direccion d\n"
+                    + " ON \n"
+                    + " d.dir_codigo = dp.dir_domicilio\n"
+                    + " LEFT JOIN  hijos h\n"
+                    + " ON h.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN persona p1\n"
+                    + " ON p1.persona_codigo = h.persona_codigo\n"
+                    + " LEFT JOIN \n"
+                    + " institucion_educativa ie\n"
+                    + " ON ie.inst_codigo = h.institucion_codigo\n"
+                    + " LEFT JOIN registro_referencia rr\n"
+                    + " ON\n"
+                    + " v.victima_codigo = rr.victima_codigo\n"
+                    + " LEFT  JOIN x_registro_agresor xra\n"
+                    + " ON\n"
+                    + " xra.registroreferencia_codigo =rr.registroreferencia_codigo \n"
+                    + "LEFT JOIN \n"
+                    + " agresor a\n"
+                    + " ON a.agresor_codigo = xra.agresor_codigo\n"
+                    + "/*WHERE extract (year from i.ingreso_fecha) = 2017*/\n"
+                    + " ORDER BY \n"
+                    + " v.victima_codigo, i.ingreso_fecha";
+
+            try {
+                ResultSet res = conn.query(SQL_SELECT);
+                while (res.next()) {
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(1), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(2), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(3), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(4), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(5), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(6), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(7), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(8), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(9), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(10), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(11), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(12), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(13), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(14), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(15), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(16), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(17), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(18), FontFactory.getFont("Arial", 9))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(19), FontFactory.getFont("Arial", 9))));
+                }
+                doc.add(tabla);
+                doc.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+            JOptionPane.showMessageDialog(null, "Documento guardado");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    public void ReporteGeneralVictima() {
+        String titulo = "Reporte General de las compañeras acogidas";
+        String[] cabecera = {"Nombre", "F.Ingreso", "F.Egreso", "Agresor", "Nacionalidad", "Provincia ", "Ciudad", "Parroquia",
+            "Años", "Instruccion", "Ocupación", "Esado Civil", "#NNA"};
+        PdfPTable tabla = createTable(cabecera, 13);
+
+        try {
+            Document doc = createDocument();
+            doc.add(Ptittle(titulo));
+//            doc.add(new Phrase());
+
+            String SQL_SELECT = " SELECT\n"
+                    + " p.persona_nombre||' '|| p.persona_apellido AS \"Compañera\",\n"
+                    + " (CASE\n"
+                    + " WHEN  i.ingreso_fecha  IS NULL THEN ''\n"
+                    + " ELSE i.ingreso_fecha||''\n"
+                    + " END) AS \"Fecha Ingreso\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  e.egreso_fecha  IS NULL THEN ''\n"
+                    + " ELSE e.egreso_fecha||''\n"
+                    + " END) AS \"Fecha Egreso\" ,\n"
+                    + " (select par_valores->xra.parentesco->>'valor' from parametros where par_id = 9) AS \"Agresor\",\n"
+                    + " (select par_valores->p.persona_nacionalidad->>'valor' from parametros where par_id = 3) AS \"Nacionalidad\",\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_provincia  IS NULL THEN ''\n"
+                    + " ELSE d.dir_provincia||''\n"
+                    + " END) AS \"Provincia\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_ciudad  IS NULL THEN ''\n"
+                    + " ELSE d.dir_ciudad||''\n"
+                    + " END) AS \"Ciudad\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN d.dir_parroquia  IS NULL THEN ''\n"
+                    + " ELSE d.dir_parroquia||''\n"
+                    + " END) AS \"Parroquia\" ,\n"
+                    + " (CASE\n"
+                    + " WHEN  ((current_date-p.persona_fecha_nac)/365)  IS NULL THEN ''\n"
+                    + " ELSE ((current_date-p.persona_fecha_nac)/365)||''\n"
+                    + " END) AS \"Años\" ,\n"
+                    + " (select par_valores->p.persona_nivel_acad->>'valor' from parametros where par_id = 5) AS \"Instruccion\",\n"
+                    + " (select par_valores->p.persona_ocupacion->>'valor' from parametros where par_id = 7) AS \"Ocupacion\",\n"
+                    + " (select par_valores->p.persona_estadocivil->>'valor' from parametros where par_id = 4) AS \"Estado Civil\",\n"
+                    + " (SELECT\n"
+                    + " COUNT(*)\n"
+                    + " FROM\n"
+                    + " hijos h \n"
+                    + " WHERE\n"
+                    + " h.victima_codigo =v.victima_codigo) AS \"NNA Responsables\"\n"
+                    + " FROM\n"
+                    + " persona p\n"
+                    + " JOIN \n"
+                    + " victima v\n"
+                    + " ON \n"
+                    + " v.persona_codigo = p.persona_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " ingreso i\n"
+                    + " ON \n"
+                    + " i.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " egreso e\n"
+                    + " ON\n"
+                    + " e.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN\n"
+                    + " direccion_persona dp\n"
+                    + " ON\n"
+                    + " p.persona_codigo = dp.persona_codigo\n"
+                    + " LEFT JOIN \n"
+                    + " direccion d\n"
+                    + " ON \n"
+                    + " d.dir_codigo = dp.dir_domicilio\n"
+                    + " LEFT JOIN  hijos h\n"
+                    + " ON h.victima_codigo = v.victima_codigo\n"
+                    + " LEFT JOIN persona p1\n"
+                    + " ON p1.persona_codigo = h.persona_codigo\n"
+                    + " LEFT JOIN \n"
+                    + " institucion_educativa ie\n"
+                    + " ON ie.inst_codigo = h.institucion_codigo\n"
+                    + " LEFT JOIN registro_referencia rr\n"
+                    + " ON\n"
+                    + " v.victima_codigo = rr.victima_codigo\n"
+                    + " LEFT  JOIN x_registro_agresor xra\n"
+                    + " ON\n"
+                    + " xra.registroreferencia_codigo =rr.registroreferencia_codigo \n"
+                    + "LEFT JOIN \n"
+                    + " agresor a\n"
+                    + " ON a.agresor_codigo = xra.agresor_codigo\n"
+                    + " /*WHERE extract (year from i.ingreso_fecha) = 2017*/\n"
+                    + " ORDER BY \n"
+                    + " v.victima_codigo, i.ingreso_fecha";
+
+            try {
+                ResultSet res = conn.query(SQL_SELECT);
+
+                while (res.next()) {
+                    //Se añaden los datos obtenidos de la base de datos a la tabla
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(1), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(2), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(3), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(4), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(5), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(6), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(7), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(8), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(9), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(10), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(11), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(12), FontFactory.getFont("Arial", 10))));
+                    tabla.addCell(new PdfPCell(new Paragraph(res.getString(13), FontFactory.getFont("Arial", 10))));
+                }
+                doc.add(tabla);
+                // Se cierra el documento
+                doc.close();
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+
+            JOptionPane.showMessageDialog(null, "Documento guardado en:");
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, "Error");
+        }
     }
 
     public Document createDocument() {
@@ -269,15 +544,14 @@ public class ControlReporte implements ActionListener {
         title.setAlignment(Element.ALIGN_CENTER);
         return title;
     }
-    
-     public PdfPTable createTable(String[] cabecera, int filas) {
+
+    public PdfPTable createTable(String[] cabecera, int filas) {
         PdfPTable tabla = new PdfPTable(filas);
-        tabla.setWidthPercentage(100);
+        tabla.setWidthPercentage(105);
         for (int i = 0; i < cabecera.length; i++) {
             tabla.addCell(new PdfPCell(new Paragraph(cabecera[i], FontFactory.getFont("Arial", 10, Font.BOLD))));
         }
         return tabla;
-
     }
 
     //Para pruebas
