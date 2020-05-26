b62224c4c0621e897d13c2a6cecda2572b97bb6d
Xavier Yanza <xavier.y.2013@gmail.com>
2020-05-26 01:06:58 -0500

    Bitacora:metodos y pruebas2

--- marylove/src/marylove/controlador/ControladorBitacora.java
+++ marylove/src/marylove/controlador/ControladorBitacora.java
@@ -27,18 +27,15 @@ import marylove.vista.VistaBitacora;
  * @author usuario
  */
 public class ControladorBitacora implements ActionListener {
-
+    
     VistaBitacora vbitacora;
-    ConexionHi con = new ConexionHi();
     BitacoraDB model = new BitacoraDB();
-    Bitacora bitacora = new Bitacora();
-    boolean validacion = false;
-
+    
     public ControladorBitacora(VistaBitacora vbitacora) {
         this.vbitacora = vbitacora;
-//        this.vbitacora.setVisible(true);
-//        this.vbitacora.setResizable(false);
-//        this.vbitacora.setLocationRelativeTo(null);
+        this.vbitacora.setVisible(true);
+        this.vbitacora.setResizable(false);
+        this.vbitacora.setLocationRelativeTo(null);
         this.vbitacora.getPnlVictima().setVisible(false);
         this.vbitacora.getPnlDescripcion().setVisible(false);
         this.vbitacora.getPnlBotones().setVisible(false);
@@ -46,15 +43,15 @@ public class ControladorBitacora implements ActionListener {
         this.vbitacora.getBtnCancelar().addActionListener(this);
         this.vbitacora.getBtnGuardar().addActionListener(this);
         this.vbitacora.getTxtFecha().setText(Fecha());
-
+        
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(vbitacora.getBtnBuscar())) {
-
-            BuscarVictima(this.vbitacora.getTxtBuscarCedula().getText().toString());
-            if (validacion) {
+            
+            model.BuscarVictima(this.vbitacora.getTxtBuscarCedula().getText().toString(), vbitacora);
+            if (model.isValidacion()) {
                 this.vbitacora.getPnlVictima().setVisible(true);
                 this.vbitacora.getPnlDescripcion().setVisible(true);
                 this.vbitacora.getPnlBotones().setVisible(true);
@@ -62,21 +59,25 @@ public class ControladorBitacora implements ActionListener {
                 this.vbitacora.getPnlVictima().setVisible(false);
                 this.vbitacora.getPnlDescripcion().setVisible(false);
                 this.vbitacora.getPnlBotones().setVisible(false);
-                JOptionPane.showMessageDialog(vbitacora, "No existe una persona ingresada con esa cédula");
+                JOptionPane.showMessageDialog(vbitacora, "No existe una persona ingresada con esa cédula", "Problema", JOptionPane.ERROR_MESSAGE);
             }
-
+            
             date();
-
+            
         }
         if (e.getSource().equals(this.vbitacora.getBtnCancelar())) {
             this.vbitacora.dispose();
         }
         if (e.getSource().equals(this.vbitacora.getBtnGuardar())) {
-            bitacora.setBitacora_desc(this.vbitacora.getTxaDescripcion().getText());
+           if(this.vbitacora.getTxaDescripcion().getText().equals("")){
+                 JOptionPane.showMessageDialog(vbitacora, "Agregue una descripción de la Actividad", "Problema", JOptionPane.ERROR_MESSAGE);
+           }else{
+                model.setBitacora_desc(this.vbitacora.getTxaDescripcion().getText());
             createBitacora();
+           }
         }
     }
-
+    
     public String Fecha() {
         String fecha;
         String pattern = "MM-dd-YYYY ";
@@ -84,7 +85,7 @@ public class ControladorBitacora implements ActionListener {
         fecha = formato.format(new Date());
         return fecha;
     }
-
+    
     public Date date() {
         String fecha = this.vbitacora.getTxtFecha().getText();
         String pattern = "dd/MM/YYYY ";
@@ -92,45 +93,54 @@ public class ControladorBitacora implements ActionListener {
         Date date = new Date();
         System.out.println(date);
         return date;
-
+        
     }
 
-    public void BuscarVictima(String cedula) {
-        try {
-            String SQL_SELECT = "SELECT\n"
-                    + "p.persona_cedula,\n"
-                    + "p.persona_nombre ||' '||p.persona_apellido,\n"
-                    + "v.victima_codigo\n "
-                    + "FROM persona p\n"
-                    + "JOIN victima v\n"
-                    + "ON v.persona_codigo=p.persona_codigo\n"
-                    + "WHERE p.persona_cedula = '" + cedula + "'";
-            validacion = false;
-            ResultSet rs = con.query(SQL_SELECT);
-
-            while (rs.next()) {
-                vbitacora.getTxtCedula().setText(rs.getString(1));
-                vbitacora.getTxtVictima().setText(rs.getString(2));
-                bitacora.setVictima_codigo(rs.getInt(3));
-                System.out.println(bitacora.getVictima_codigo());
-                validacion = true;
-            }
-            System.out.println(rs);
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex);
-        }
-
-    }
-
+//    public void BuscarVictima(String cedula) {
+//        try {
+//            String SQL_SELECT = "SELECT\n"
+//                    + "p.persona_cedula,\n"
+//                    + "p.persona_nombre ||' '||p.persona_apellido,\n"
+//                    + "v.victima_codigo\n "
+//                    + "FROM persona p\n"
+//                    + "JOIN victima v\n"
+//                    + "ON v.persona_codigo=p.persona_codigo\n"
+//                    + "WHERE p.persona_cedula = '" + cedula + "'";
+//            validacion = false;
+//            ResultSet rs = con.query(SQL_SELECT);
+//
+//            while (rs.next()) {
+//                vbitacora.getTxtCedula().setText(rs.getString(1));
+//                vbitacora.getTxtVictima().setText(rs.getString(2));
+//                bitacora.setVictima_codigo(rs.getInt(3));
+//                System.out.println(bitacora.getVictima_codigo());
+//                validacion = true;
+//            }
+//            System.out.println(rs);
+//
+//        } catch (SQLException ex) {
+//            JOptionPane.showMessageDialog(null, ex);
+//        }
+//
+//    }
     public void createBitacora() {
-        String SQL_INSERT = "INSERT INTO bitacora (personal_codigo,bitacora_date,bitacora_desc,victima_codigo)\n"
-                + "VALUES(1,'05-25-2020','" + bitacora.getBitacora_desc() + "'," + bitacora.getVictima_codigo() + ")";
+//        String SQL_INSERT = "INSERT INTO bitacora (personal_codigo,bitacora_date,bitacora_desc,victima_codigo)\n"
+//                + "VALUES(1,'05-25-2020','" + bitacora.getBitacora_desc() + "'," + bitacora.getVictima_codigo() + ")";
         try {
-            con.noQuery(SQL_INSERT);
+//            con.noQuery(SQL_INSERT);
+            if (model.crearBitacora()) {
+                JOptionPane.showMessageDialog(vbitacora, "Datos Ingresados Correctamente", "Información", JOptionPane.INFORMATION_MESSAGE);
+                this.vbitacora.getTxtBuscarCedula().setText("");
+                this.vbitacora.getPnlVictima().setVisible(false);
+                this.vbitacora.getPnlDescripcion().setVisible(false);
+                this.vbitacora.getPnlBotones().setVisible(false);
+            } else {
+                JOptionPane.showMessageDialog(vbitacora, "Agregue una descripción de la Actividad", "Problema", JOptionPane.ERROR_MESSAGE);
+                
+            }
         } catch (Exception e) {
             System.out.println("ERROOOOOR> " + e);
         }
     }
-
+    
 }
