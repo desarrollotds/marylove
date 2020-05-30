44a013a17883268696f03c3ffae4b02a0ed24317
Danicho23 <henry.brito4040@gmail.com>
2020-05-29 21:05:01 -0500

    eliminar linea
    no info

--- marylove/src/marylove/controlador/ControladorFichaAnamnesis.java
+++ marylove/src/marylove/controlador/ControladorFichaAnamnesis.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Preloader;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
