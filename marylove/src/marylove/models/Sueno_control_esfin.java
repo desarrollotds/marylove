321385f560a2d3aded85d2f351e0e6622af14087
CamiloT777 <camilot774@gmail.com>
2020-09-15 23:53:06 -0500

    Correccion de dato
    Se realizo la modificacion del dato "falta de control del esfínter anal" en la pestaña "sueño control de esfínteres" en la ficha anamnesis. El cambio fue exitoso, actualmente los datos se guardan correctamente. Pendiente la consulta del dato.

--- marylove/src/marylove/models/Sueno_control_esfin.java
+++ marylove/src/marylove/models/Sueno_control_esfin.java
@@ -14,15 +14,15 @@ public class Sueno_control_esfin extends Desarrollo{
     private int edad_control_esfinter;
     private boolean ayuda_bano;
     private boolean moja_cama;
-    private String periodo_ecopresis_descrip;
-    private boolean periodo_ecopresis;
+    private String incontrol_esfin_anal_descrip;
+    private boolean incontrol_esfin_anal;
     private String como_es_sueno;
     private String acompanamiento_dormir;
     private boolean sueno_cont_estado;
 
     public Sueno_control_esfin() {
     }
-    public Sueno_control_esfin(int sucoes_id, boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String periodo_ecopresis_descrip, boolean periodo_ecopresis, String como_es_sueno, String acompanamiento_dormir, boolean sueno_cont_estado, String des_motor_grueso, String des_motor_fino, String movimientos, String des_psico_social, String des_cognitivo, String des_fisico, String caridad_lenguajes, String claridad_lenguajes_descrip, boolean desarrollo_estado) {
+    public Sueno_control_esfin(int sucoes_id, boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String incontrol_esfin_anal_descrip, boolean incontrol_esfin_anal, String como_es_sueno, String acompanamiento_dormir, boolean sueno_cont_estado, String des_motor_grueso, String des_motor_fino, String movimientos, String des_psico_social, String des_cognitivo, String des_fisico, String caridad_lenguajes, String claridad_lenguajes_descrip, boolean desarrollo_estado) {
         super(des_motor_grueso, des_motor_fino, movimientos, des_psico_social, des_cognitivo, des_fisico, caridad_lenguajes, claridad_lenguajes_descrip, desarrollo_estado);
         this.sucoes_id = sucoes_id;
         this.duerme_toda_noche = duerme_toda_noche;
@@ -32,14 +32,14 @@ public class Sueno_control_esfin extends Desarrollo{
         this.edad_control_esfinter = edad_control_esfinter;
         this.ayuda_bano = ayuda_bano;
         this.moja_cama = moja_cama;
-        this.periodo_ecopresis_descrip = periodo_ecopresis_descrip;
-        this.periodo_ecopresis = periodo_ecopresis;
+        this.incontrol_esfin_anal_descrip = incontrol_esfin_anal_descrip;
+        this.incontrol_esfin_anal = incontrol_esfin_anal;
         this.como_es_sueno = como_es_sueno;
         this.acompanamiento_dormir = acompanamiento_dormir;
         this.sueno_cont_estado = sueno_cont_estado;
     }
 
-    public Sueno_control_esfin(boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String periodo_ecopresis_descrip, boolean periodo_ecopresis, String como_es_sueno, String acompanamiento_dormir) {
+    public Sueno_control_esfin(boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String incontrol_esfin_anal_descrip, boolean incontrol_esfin_anal, String como_es_sueno, String acompanamiento_dormir) {
         this.duerme_toda_noche = duerme_toda_noche;
         this.miedo_dormir_solo = miedo_dormir_solo;
         this.despertar_descripcion = despertar_descripcion;
@@ -47,13 +47,13 @@ public class Sueno_control_esfin extends Desarrollo{
         this.edad_control_esfinter = edad_control_esfinter;
         this.ayuda_bano = ayuda_bano;
         this.moja_cama = moja_cama;
-        this.periodo_ecopresis_descrip = periodo_ecopresis_descrip;
-        this.periodo_ecopresis = periodo_ecopresis;
+        this.incontrol_esfin_anal_descrip = incontrol_esfin_anal_descrip;
+        this.incontrol_esfin_anal = incontrol_esfin_anal;
         this.como_es_sueno = como_es_sueno;
         this.acompanamiento_dormir = acompanamiento_dormir;
     }
 
-    public Sueno_control_esfin(int sucoes_id, boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String periodo_ecopresis_descrip, boolean periodo_ecopresis, String como_es_sueno, String acompanamiento_dormir) {
+    public Sueno_control_esfin(int sucoes_id, boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String incontrol_esfin_anal_descrip, boolean incontrol_esfin_anal, String como_es_sueno, String acompanamiento_dormir) {
         this.sucoes_id = sucoes_id;
         this.duerme_toda_noche = duerme_toda_noche;
         this.miedo_dormir_solo = miedo_dormir_solo;
@@ -62,13 +62,13 @@ public class Sueno_control_esfin extends Desarrollo{
         this.edad_control_esfinter = edad_control_esfinter;
         this.ayuda_bano = ayuda_bano;
         this.moja_cama = moja_cama;
-        this.periodo_ecopresis_descrip = periodo_ecopresis_descrip;
-        this.periodo_ecopresis = periodo_ecopresis;
+        this.incontrol_esfin_anal_descrip = incontrol_esfin_anal_descrip;
+        this.incontrol_esfin_anal = incontrol_esfin_anal;
         this.como_es_sueno = como_es_sueno;
         this.acompanamiento_dormir = acompanamiento_dormir;
     }
 
-    public Sueno_control_esfin(boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String periodo_ecopresis_descrip, boolean periodo_ecopresis, String como_es_sueno, String acompanamiento_dormir, boolean sueno_cont_estado) {
+    public Sueno_control_esfin(boolean duerme_toda_noche, boolean miedo_dormir_solo, String despertar_descripcion, boolean pesadillas, int edad_control_esfinter, boolean ayuda_bano, boolean moja_cama, String incontrol_esfin_anal_descrip, boolean incontrol_esfin_anal, String como_es_sueno, String acompanamiento_dormir, boolean sueno_cont_estado) {
         this.duerme_toda_noche = duerme_toda_noche;
         this.miedo_dormir_solo = miedo_dormir_solo;
         this.despertar_descripcion = despertar_descripcion;
@@ -76,8 +76,8 @@ public class Sueno_control_esfin extends Desarrollo{
         this.edad_control_esfinter = edad_control_esfinter;
         this.ayuda_bano = ayuda_bano;
         this.moja_cama = moja_cama;
-        this.periodo_ecopresis_descrip = periodo_ecopresis_descrip;
-        this.periodo_ecopresis = periodo_ecopresis;
+        this.incontrol_esfin_anal_descrip = incontrol_esfin_anal_descrip;
+        this.incontrol_esfin_anal = incontrol_esfin_anal;
         this.como_es_sueno = como_es_sueno;
         this.acompanamiento_dormir = acompanamiento_dormir;
         this.sueno_cont_estado = sueno_cont_estado;
@@ -147,20 +147,20 @@ public class Sueno_control_esfin extends Desarrollo{
         this.moja_cama = moja_cama;
     }
 
-    public String getPeriodo_ecopresis_descrip() {
-        return periodo_ecopresis_descrip;
+    public String getIncontrol_esfin_anal_descrip() {
+        return incontrol_esfin_anal_descrip;
     }
 
-    public void setPeriodo_ecopresis_descrip(String periodo_ecopresis_descrip) {
-        this.periodo_ecopresis_descrip = periodo_ecopresis_descrip;
+    public void setIncontrol_esfin_anal_descrip(String incontrol_esfin_anal_descrip) {
+        this.incontrol_esfin_anal_descrip = incontrol_esfin_anal_descrip;
     }
 
-    public boolean isPeriodo_ecopresis() {
-        return periodo_ecopresis;
+    public boolean isIncontrol_esfin_anal() {
+        return incontrol_esfin_anal;
     }
 
-    public void setPeriodo_ecopresis(boolean periodo_ecopresis) {
-        this.periodo_ecopresis = periodo_ecopresis;
+    public void setIncontrol_esfin_anal(boolean incontrol_esfin_anal) {
+        this.incontrol_esfin_anal = incontrol_esfin_anal;
     }
 
     public String getComo_es_sueno() {
